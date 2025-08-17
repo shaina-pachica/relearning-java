cf57995e868475f3c420896030e906be93f72ada
Shaina Pachica <shainapachica@gmail.com>
2025-08-18 00:20:39 +0800

    Simplified the problem (c) CodeAcademy (c) demonstrating Object-Oriented Programming

--- Basics/ObjectOrientedProgramming/Methods/SavingsAccount/SavingsAccount.java
+++ Basics/ObjectOrientedProgramming/Methods/SavingsAccount/SavingsAccount.java
@@ -1,8 +1,28 @@
 public class SavingsAccount {
-  
-  int balance;
-    
-  public SavingsAccount(int initialBalance){
-    balance = initialBalance;
-  }
+
+    int balance;
+
+    public SavingsAccount(int initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void checkBalance() {
+        System.out.println("\nHello! \nYour balance is currently " + balance + ".\n");
+    }
+
+    public void withdraw(int withdrawAmount) {
+        int afterWithdraw = balance - withdrawAmount;
+        balance = afterWithdraw;
+        System.out.print(withdrawAmount + " was deducted to your Savings Account balance. Happy to serve you!\n");
+    }
+
+    public void deposit(int depositAmount) {
+        int afterDeposit = balance + depositAmount;
+        balance = afterDeposit;
+        System.out.println(depositAmount + " was added to your Savings Account balance. Happy to serve you!\n");
+    }
+
+    public String toString() {
+        return "\nThis is a Savings Account (SA) with " + balance + " saved.\n";
+    }
 }
